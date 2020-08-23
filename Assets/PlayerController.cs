8231c45fb1e3b06e3677202a4bb18481d8b1d867
rpparede <rpparede@uwaterloo.ca>
2020-08-22 23:01:52 -0700

    Walking animations added

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public Animator anim;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,21 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
+      if(Input.GetKey(KeyCode.A)){
+        rb.velocity = new Vector2(-5, rb.velocity.y);
+        transform.localScale = new Vector2(-1, 1);
+        anim.SetBool("running", true);
+      } else if(Input.GetKey(KeyCode.D)){
+        rb.velocity = new Vector2(5, rb.velocity.y);
+        transform.localScale = new Vector2(1, 1);
+        anim.SetBool("running", true);
+      } else {
+        anim.SetBool("running", false);
+      }
+
+      if(Input.GetKeyDown(KeyCode.Space)){
+        rb.velocity = new Vector2(rb.velocity.x, 10f);
+      }
     }
+
 }
